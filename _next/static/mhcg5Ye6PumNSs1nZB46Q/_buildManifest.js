0f527c677aeeef21ed81e3856d9961ba291b077f
persteenolsen <38952136+persteenolsen@users.noreply.github.com>
2024-12-01 11:24:05 +0000

    Deploying to gh-pages from @ persteenolsen/gh-actions-pages-next-js-one@1ea59cf58a8e2b6029416c9b518d05041a8f7fea 🚀

--- _next/static/mhcg5Ye6PumNSs1nZB46Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/48d4b1c8601bfd92.css","static/chunks/pages/index-8d799c673af57f08.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
